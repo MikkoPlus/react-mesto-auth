a7aedeebcb94fb593794017e880f119b5e526446
Anatoliy Sharov <sharov.012@gmail.com>
2023-07-13 16:20:53 +0300

    add scc to tooltip icon

--- src/components/Popups/InfoTooltip.jsx
+++ src/components/Popups/InfoTooltip.jsx
@@ -1,23 +1,36 @@
-import Popup from "./Popup";
-import accessIcon from "../../images/icons/access.svg";
-import accessDeniedIcon from "../../images/icons/access-denied.svg";
+import Popup from './Popup';
+import { useState, useEffect } from 'react';
 
 function InfoTooltip({ accessStatus, isOpen, onClose }) {
-  const tooltipIcon = accessStatus ? accessIcon : accessDeniedIcon;
-  const tooltipAlt = accessStatus ? "access" : "access denied";
-  const tooltipText = accessStatus
-    ? "Вы успешно зарегистрировались!"
-    : "Что-то пошло не так! Попробуйте ещё раз.";
+  const [tooltipText, setTooltipText] = useState('');
+  const imageClassName = `popup__info-img ${
+    accessStatus
+      ? 'popup__info-img_type_access'
+      : 'popup__info-img_type_access-denied'
+  }`;
+
+  useEffect(() => {
+    if (accessStatus) {
+      setTooltipText('Вы успешно зарегистрировались!');
+    } else {
+      setTooltipText('Что-то пошло не так! Попробуйте ещё раз.');
+    }
+
+    return () => {
+      setTooltipText('');
+    };
+  }, [accessStatus]);
 
   return (
     <Popup
-      popupClass="info-tooltip"
-      type="info-tooltip"
+      popupClass='info-tooltip'
+      type='info-tooltip'
       isOpen={isOpen}
       onClose={onClose}
     >
-      <img className="popup__info-img" src={tooltipIcon} alt={tooltipAlt} />
-      <p className="popup__info-title">{tooltipText}</p>
+      <div className={imageClassName} />
+
+      <p className='popup__info-title'>{tooltipText}</p>
     </Popup>
   );
 }
